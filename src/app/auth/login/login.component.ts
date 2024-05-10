e0a42ef1c51b36947de605b8a64f230fe05ebc1a
Vishal Umtol <vau4869@gmail.com>
2024-05-11 04:10:22 +0530

    login service partially completed

--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -1,23 +1,40 @@
-import { Component } from '@angular/core';
-import { LayoutService } from 'src/app/layout/service/app.layout.service';
+import { Component, OnInit } from '@angular/core';
+import { AuthService } from 'src/app/@service/auth.service';
+import { FormBuilder, FormGroup, FormControl } from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
-    selector: 'app-login',
-    templateUrl: './login.component.html',
-    styles: [`
-        :host ::ng-deep .pi-eye,
-        :host ::ng-deep .pi-eye-slash {
-            transform:scale(1.6);
-            margin-right: 1rem;
-            color: var(--primary-color) !important;
-        }
-    `]
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrl: './login.component.scss'
 })
-export class LoginComponent {
+export class LoginComponent implements OnInit {
 
-    valCheck: string[] = ['remember'];
+  loginForm = this.fb.group({
+    username: this.fb.control(''),
+    password: this.fb.control('')
+  });
 
-    password!: string;
+  constructor(
+    private fb: FormBuilder,
+    private loginService: AuthService,
+    private router: Router
+  ) { }
 
-    constructor(public layoutService: LayoutService) { }
+  ngOnInit() {
+  }
+
+  onLogin(loginForm: FormGroup) {
+    this.loginService.login(loginForm.value.username, loginForm.value.password).subscribe((res: any) => {
+      console.log(res);
+      if(res.token) {
+        localStorage.setItem('access_token', res.token);
+        this.loginService.me().subscribe((res: any) => {
+          localStorage.setItem('loggedInUser', JSON.stringify(res));
+          this.router.navigate(['/']);
+        });
+      }
+    });
+  }
+
 }
