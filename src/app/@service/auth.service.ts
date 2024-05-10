94ef10e9cb62eb75e1509d84ee34e96d0b741f01
Vishal Umtol <vau4869@gmail.com>
2024-05-11 04:27:14 +0530

    logout logic partially added

--- src/app/@service/auth.service.ts
+++ src/app/@service/auth.service.ts
@@ -3,6 +3,8 @@ import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { environment } from 'src/environments/environment';
 import {catchError, map} from 'rxjs/operators';
+import { Router } from '@angular/router';
+
 
 const API_URL=environment.API_URL;
 
@@ -13,7 +15,9 @@ const headersData={ Authorization: `Bearer ${localStorage.getItem('access_token'
   })
 export class AuthService {
 
-    constructor(private http: HttpClient) { }
+    constructor(private http: HttpClient,
+        private router: Router
+        ) { }
 
     login(username: string, password: string) {
         return this.http.post(`${API_URL}/auth/login`, { username, password });
@@ -21,5 +25,12 @@ export class AuthService {
     
     me(){
         return this.http.get(`${API_URL}/auth/me`, { headers: { Authorization: `Bearer ${localStorage.getItem('access_token')}` } });
+    }
+
+    logout() {
+        // console.log(localStorage);
+        localStorage.removeItem('access_token');
+        localStorage.removeItem('loggedInUser');
+        this.router.navigate(['/'])
     }
 }
